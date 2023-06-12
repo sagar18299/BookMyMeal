dec131036e39554550e5b6a94c9a881f6e439443
Sagar Sharma <sagar18299@gmail.com>
2023-06-12 23:19:16 +0530

    remove socket

--- front end/my-app/src/helpers/authHelper.js
+++ front end/my-app/src/helpers/authHelper.js
@@ -1,20 +1,10 @@
-import { initiateSocketConnection } from "./socketHelper";
 import { AUTH_TOKEN } from "./constants";
 
 const isLoggedIn = () => {
   return localStorage.getItem(AUTH_TOKEN);
 };
 
-const loginUser = (user) => {
-  localStorage.setItem("user", JSON.stringify(user));
-  initiateSocketConnection();
-};
-
-const logoutUser = () => {
-  localStorage.removeItem("user");
-  initiateSocketConnection();
-};
 
 const getAccessToken = () => localStorage.getItem(AUTH_TOKEN);
 
-export { loginUser, isLoggedIn, logoutUser, getAccessToken };
+export { isLoggedIn, getAccessToken };
